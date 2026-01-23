a8de8dc8d20d6918a493a9666e9110303b96078c
bactxse180308 <bactxse180308@fpt.edu.com>
2026-01-23 07:27:20 +0700

    Init project

--- src/main/java/com/sba302/electroshop/config/SecurityConfig.java
+++ src/main/java/com/sba302/electroshop/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.sba302.electroshop.config;
 
+import com.sba302.electroshop.security.JwtAuthFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
