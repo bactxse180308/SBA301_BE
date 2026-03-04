b770b1086db9d5f34ed95cd4e6d9cb0e48d578bb
thienddse180316 <thienddse180316@fpt.edu.vn>
2026-03-04 21:38:36 +0700

    implement cart order voucher flow

--- src/main/java/com/sba302/electroshop/repository/CartItemRepository.java
+++ src/main/java/com/sba302/electroshop/repository/CartItemRepository.java
@@ -4,9 +4,13 @@ import com.sba302.electroshop.entity.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CartItemRepository extends JpaRepository<CartItem, Integer> {
+    List<CartItem> findByCart_CartId(Integer cartId);
+    Optional<CartItem> findByCart_CartIdAndProduct_ProductId(Integer cartId, Integer productId);
+    void deleteByCart_CartId(Integer cartId);
 
 }
--- src/main/java/com/sba302/electroshop/repository/ShoppingCartRepository.java
+++ src/main/java/com/sba302/electroshop/repository/ShoppingCartRepository.java
@@ -4,6 +4,9 @@ import com.sba302.electroshop.entity.ShoppingCart;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, Integer> {
+    Optional<ShoppingCart> findByUser_UserId(Integer userId);
 }
