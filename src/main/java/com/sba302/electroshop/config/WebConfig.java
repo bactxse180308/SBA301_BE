248614babfceeb506945cb8db63d2cb942ec986b
yakimo123 <hunglqse180319@fpt.edu.vn>
2026-03-08 11:38:28 +0700

    fix: add  PATCH methods in cors and add setting for pageble generate dto

---
+++ src/main/java/com/sba302/electroshop/config/WebConfig.java
@@ -0,0 +1,11 @@
+package com.sba302.electroshop.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+@Configuration
+@EnableSpringDataWebSupport(
+        pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO
+)
+public class WebConfig {
+}
